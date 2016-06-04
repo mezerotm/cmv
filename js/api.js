242606a7a98260a2111b063c19e9e4d9b972b0d7
njtuley <njtuley@aol.com>
2016-06-04 15:33:35 -0400

    sidebar opening function and closing functions completed and bugs fixed

--- js/api.js
+++ js/api.js
@@ -91,20 +91,32 @@ function initMap() {
 
 
 /* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
-    document.getElementById("mySidenav").style.border = "1px solid black";
+function openNav1() {
+    closeNav();
+    document.getElementById("mySidenav1").style.width = "300px";
+    document.getElementById("mySidenav1").style.border = "1px solid black";
+
 }
 
-/* Set the width of the side navigation to 0 */
+function openNav2() {
+    closeNav();
+    document.getElementById("mySidenav2").style.width = "300px";
+    document.getElementById("mySidenav2").style.border = "1px solid black";
+}
+
+function openNav3() {
+    closeNav();
+    document.getElementById("mySidenav3").style.width = "300px";
+    document.getElementById("mySidenav3").style.border = "1px solid black";
+}
+
+/* Set the width of the side navigation to 0
+@param nav_num the number of the nav bar that is being closed (starts at mySidenav1) */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("mySidenav").style.border = "0px solid white";
-}
-
-/*
-    figure out which sidebar (if any) to close when a new sidebar is clicked to open
-*/
-function getClose() {
-    
+    document.getElementById("mySidenav1").style.width = "0";
+    document.getElementById("mySidenav1").style.border = "0px solid white";
+    document.getElementById("mySidenav2").style.width = "0";
+    document.getElementById("mySidenav2").style.border = "0px solid white";
+    document.getElementById("mySidenav3").style.width = "0";
+    document.getElementById("mySidenav3").style.border = "0px solid white";
 }
