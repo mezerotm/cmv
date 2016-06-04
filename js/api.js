dcb304ba9ff2a6970fa237c49a860f57730ecb4d
Charles R. Hardnett <hardnett@gmail.com>
2016-06-04 11:43:30 -0400

    Working on callback to citySDK. This uses the new CItySDK.

--- js/api.js
+++ js/api.js
@@ -1,20 +1,53 @@
-var sdk = new CitySDK(); //Create the CitySDK Instance
-var census = sdk.modules.census; //Create an instance of the module
-census.enable("cdfd4ff35b1ffdf619b567c6fa2988fd3099ead"); //Enable module with the API key
+var censusAPIKey = "cdfd4ff35b1ffdf619b567c6fa2988fd3099ead";
+var sdk = new CitySdk(); //Create the CitySDK Instance
+var census = new CensusModule(censusAPIKey); //Create an instance of the module
+sdk.modules.census = census;
 
 
 //creating the request variable. Please note that I have left in income as the default variable.
+//var request = {
+//    "level": "county",
+//    "zip": "",
+//    "variables": [
+//        
+//    ],
+//    "api": "acs5",
+//    "year": "",
+//    "tract": ""
+//};
+
 var request = {
     "level": "county",
-    "zip": "",
+    "zip": "30013",
     "variables": [
-        
+      "age"
     ],
-    "api": "acs5",
-    "year": "",
-    "tract": ""
+    "api": "acs1",
+    "year": "2012",
+    "county": "Rockdale County",
+    "state": "GA"
+  };
+
+apiCallBack = function (response) {
+    alert(response);
+    //Outputs the raw JSON text-full data
+    jQuery("#data").text(JSON.stringify(response, null, 4));
+
+    //Below outputs only income or whatever is requested. 
+   //jQuery("#data").text(JSON.stringify(+response.data[0].poverty, null, 4));
+
+    //console.log(JSON.parse(response));
+   // alert("hello");
 };
 
+
+console.log(sdk);
+console.log(census);
+census.apiRequest(request, apiCallBack);    
+     
+
+
+    
 //Function which will gather data from user and then submit it to the API. The API will then return 
 //the data that was requested. 
 function data(){
@@ -22,28 +55,23 @@ function data(){
     var year = document.getElementById("year").value;
     var variable = document.getElementById("variables").value;
     //var tract = document.getElementById("tract").value;
-    request.zip = zip;
-    request.year = year;
-    request.variables.push(variable);
+    //request.zip = zip;
+    //request.year = year;
+    //request.variables.push(variable);
+console.log(citysdk);
+  console.log(census);
+
 
     //The request to gather the actual data.
-    census.APIRequest(request, function (response) {
-
-    //Outputs the raw JSON text-full data
-    //jQuery("#data").text(JSON.stringify(response, null, 4));
-
-    //Below outputs only income or whatever is requested. 
-    jQuery("#data").text(JSON.stringify(+response.data[0].poverty, null, 4));
-
-    //console.log(JSON.parse(test));
-    //alert(parseInt(test));
-    });     
+    census.apiRequest(request, apiCallBack);   
+    
+    
 }
 
 /////////////  Sliding effect 
 
 $("#menu").click(function() {
-  $("#toggle").slideToggle(550)
+  $("#toggle").slideToggle(550);
 });
 
 ////////// Google Maps ////////////////
