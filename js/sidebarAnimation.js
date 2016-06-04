1dedc0917b4d42b49be5f2006dcadc8ccda72aa0
njtuley <njtuley@aol.com>
2016-06-04 16:32:49 -0400

    added 3 more side nav bars and made corresponding additions

--- js/sidebarAnimation.js
+++ js/sidebarAnimation.js
@@ -18,6 +18,24 @@ function openNav3() {
     document.getElementById("mySidenav3").style.border = "1px solid black";
 }
 
+function openNav4() {
+    closeNav();
+    document.getElementById("mySidenav4").style.width = "300px";
+    document.getElementById("mySidenav4").style.border = "1px solid black";
+}
+
+function openNav5() {
+    closeNav();
+    document.getElementById("mySidenav5").style.width = "300px";
+    document.getElementById("mySidenav5").style.border = "1px solid black";
+}
+
+function openNav6() {
+    closeNav();
+    document.getElementById("mySidenav6").style.width = "300px";
+    document.getElementById("mySidenav6").style.border = "1px solid black";
+}
+
 /* Set the width of the side navigation to 0
 @param nav_num the number of the nav bar that is being closed (starts at mySidenav1) */
 function closeNav() {
@@ -27,4 +45,10 @@ function closeNav() {
     document.getElementById("mySidenav2").style.border = "0px solid white";
     document.getElementById("mySidenav3").style.width = "0";
     document.getElementById("mySidenav3").style.border = "0px solid white";
+    document.getElementById("mySidenav4").style.width = "0";
+    document.getElementById("mySidenav4").style.border = "0px solid white";
+    document.getElementById("mySidenav5").style.width = "0";
+    document.getElementById("mySidenav5").style.border = "0px solid white";
+    document.getElementById("mySidenav6").style.width = "0";
+    document.getElementById("mySidenav6").style.border = "0px solid white";
 }
