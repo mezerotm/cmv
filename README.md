a01ffec6049e9ee88579c04bc1da724d6eec1672
nicholas tuley <nicholas tuley>
2016-06-04 12:01:16 -0400

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Census Map Visualization
+## Intro
+CMV is a [citySDK](http://uscensusbureau.github.io/citysdk) based map visualization tool; turning datasets from the [census bereau API](http://census.gov/data/developers/data-sets.html) into a visual representation that anyone can view using [Google Maps Javascript API](https://developers.google.com/maps/documentation/javascript/) and [D3.js](https://github.com/mbostock/d3)
+# Getting Started
+### Installing
+    git clone https://github.com/MEzeroTM/CensusMapVisualization.git
+    cd CensusMapVisualizatoin
+    npm install 
+    
---
+++ js/api.js
@@ -0,0 +1,58 @@
+var sdk = new CitySDK(); //Create the CitySDK Instance
+var census = sdk.modules.census; //Create an instance of the module
+census.enable("24e658711bf26dbead500bf24f3f522fd50a79a9
+"); //Enable module with the API key
+
+
+//creating the request variable. Please note that I have left in income as the default variable.
+var request = {
+    "level": "county",
+    "zip": "",
+    "variables": [
+        
+    ],
+    "api": "acs5",
+    "year": "",
+    "tract": ""
+};
+
+//Function which will gather data from user and then submit it to the API. The API will then return 
+//the data that was requested. 
+function data(){
+    var zip = document.getElementById("zip").value;
+    var year = document.getElementById("year").value;
+    var variable = document.getElementById("variables").value;
+    //var tract = document.getElementById("tract").value;
+    request.zip = zip;
+    request.year = year;
+    request.variables.push(variable);
+
+    //The request to gather the actual data.
+    census.APIRequest(request, function (response) {
+
+    //Outputs the raw JSON text-full data
+    //jQuery("#data").text(JSON.stringify(response, null, 4));
+
+    //Below outputs only income or whatever is requested. 
+    jQuery("#data").text(JSON.stringify(+response.data[0].poverty, null, 4));
+
+    //console.log(JSON.parse(test));
+    //alert(parseInt(test));
+    });     
+}
+
+/////////////  Sliding effect 
+
+$("#menu").click(function() {
+  $("#toggle").slideToggle(550)
+});
+
+////////// Google Maps ////////////////
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 34.1207, lng: -84.0044},
+    zoom: 10,
+    scrollwheel: false
+    });
+}
