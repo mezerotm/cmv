3243b5d32624ee36c9193ed61230d136f6f68119
njtuley <njtuley@aol.com>
2016-06-04 14:39:00 -0400

    style changes for sidebar animation

--- js/api.js
+++ js/api.js
@@ -85,3 +85,17 @@ function initMap() {
     scrollwheel: false
     });
 }
+
+
+/*   SPACER    */
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
