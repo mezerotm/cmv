6039ee07407d40e3339d3500cf1e83c7dcd1b348
Charles R. Hardnett <hardnett@gmail.com>
2016-08-28 07:58:06 -0400

    Merged api.js file Data mapping support and polygon drawing are merged.

--- js/api.js
+++ js/api.js
@@ -20,11 +20,12 @@
 // false: debug output off
 var DEBUG = true; 
 
-
 // Enable module with the API key
 var censusAPIKey = "c83e06ec87c35c0d3ffb0f6d7640afbf52b7071c";
 var sdk = new CitySdk(); //Create the CitySDK Instance
 var census = new CensusModule(censusAPIKey); //Create an instance of the module
+var censusVariables = new CensusVariablesMap(); // Create a mapping object for the census variables.
+
 //Here is where I set up and make my call to google maps API. 
 function init_map() {
       var var_location = new google.maps.LatLng(33.895,-84.210);
@@ -54,85 +55,42 @@ function init_map() {
 google.maps.event.addDomListener(window, 'load', init_map);
 
 
-
 /*
- * apiCallBack : This is callback function that responds to the responses from the 
- * basic api calls made to the City SDK.
- *       response: The JSON response from the City SDK
- */
-apiCallBack = function (response) {
-    
-    // echo results for debugging purposes
-    if (DEBUG)
-        console.log(JSON.stringify(response, null, 4));
-    
-    // process the api callback response
-    
-    //Outputs the raw JSON text-full data
-    if (DEBUG)
-        jQuery("#data").text(JSON.stringify(response, null, 4));
-};
-
-
-/*
- * geoCAllBack : This is the callback function that respondes to the responses 
+ * geoCallBack : This is the callback function that responds to the responses 
  * from the geographical based api of the City SDK.
  *        response: The JSON response from the City SDK
  */
 geoCallBack = function(response) {
-    
-    // output geo data to an output panel on the UI
-    if (DEBUG) {
-        var resultsArea = document.getElementById("mySidenav3");
-        var theResultsContent = document.createElement("p");
-        
-        theResultsContent.appendChild(document.createTextNode(JSON.stringify(response, null, 4)));
+    if (response) {
 
-        resultsArea.appendChild(theResultsContent);
+        var dataResults = JSON.stringify(response, null, 4);
+        var resultsObject = JSON.parse(dataResults);
+        processTractData(resultsObject);
+        // output geo data to an output panel on the UI
+        if (DEBUG) {
+            
+            console.log(response.features.length);
+            console.log(JSON.stringify(response, null, 4));
+        }
 
-        //Working on building coorindate array => Work is all below.
-        //console.log(response.features[1].geometry.coordinates[0][0]); 
-
-        // console.log(response.features[0].geometry.coordinates[0][0][0])
-        // console.log(response.features.length);
-
-        // arrayOfCoords = [];
-
-        // for (var i = 0; i < 184; i++){
-        //     //Creating a variable to hold the coordinates
-        //     var coord = response.features[0].geometry.coordinates[0][i];
-        //     //pushing the coordinates into the array:
-        //     arrayOfCoords.push(coord);
-        // }
-
-        //This tells us that there are 113 tracts.
-        // console.log(response.features.length);
-        // for (var tract = 0; tract < response.features.length; tract++){
-        //     for (var coord = 0; coord < response.features[tract].length; coord++){
-
-        //   }
-        // }
-
-
-        //console.log(response.features[0].geometry.coordinates[0]);
-
+       
          var map = new google.maps.Map(document.getElementById('map1'), {
             zoom: 8,
             center: {lat: 33.895, lng: -84.210},
             mapTypeId: 'terrain'
         });
-
+       
        var colors = ["red", "green", "blue"];
-  
+       
        for (var tract = 0; tract < response.features.length; tract++) {
-
+       
             var Coords = []
-        
+       
             for (var i = 0; i < response.features[tract].geometry.coordinates[0].length; i++){
                 if (i % 20 === 1) {
                     Coords.push({lat: response.features[tract].geometry.coordinates[0][i][1], lng: response.features[tract].geometry.coordinates[0][i][0]});
                 }
-
+       
                 var pickColor = Math.round((Math.random() * 10)) % colors.length
                 console.log(pickColor);
                 var polyShape = new google.maps.Polygon({
@@ -146,24 +104,24 @@ geoCallBack = function(response) {
                 polyShape.setMap(map);      
             }
         }
-
+       
         var map = new google.maps.Map(document.getElementById('map2'), {
             zoom: 8,
             center: {lat: 33.895, lng: -84.210},
             mapTypeId: 'terrain'
         });
-
+       
        var colors = ["red", "green", "blue"];
-  
+       
        for (var tract = 0; tract < response.features.length; tract++) {
-
+       
             var Coords = []
-        
+       
             for (var i = 0; i < response.features[tract].geometry.coordinates[0].length; i++){
                 if (i % 20 === 1) {
                     Coords.push({lat: response.features[tract].geometry.coordinates[0][i][1], lng: response.features[tract].geometry.coordinates[0][i][0]});
                 }
-
+       
                 var pickColor = Math.round((Math.random() * 10)) % colors.length
                 console.log(pickColor);
                 var polyShape = new google.maps.Polygon({
@@ -177,78 +135,38 @@ geoCallBack = function(response) {
                 polyShape.setMap(map);      
             }
         }
-}
-        //console.log(Coords);
-
-        //The below line gets me all of the coordinate points:
-        //console.log(response.features[0].geometry.coordinates[0]);
-        //It will pull back with two objects which contain the information that I need. 
-        //This line below will get me the coordinate location at index 0: 
-        //console.log(response.features[0].geometry.coordinates[0][0]);
-        //This will get me the array at index 1:
-        // console.log(response.features[0].geometry.coordinates[0][1]);
-
-        //Array to hold all of the coordinates that will be used for the lines of the polygon's path.
-        //arrayOfCoords = []; 
-
-        /* For loop to go through all of the coordinates. There are a total of 30148 points.
-           I would also say that I think that I could take the for loop below and 
-           simply replace it with this one. I have two for loops that are basically doing the same thing.
-           The first one builds an array of all of the coordinates. The second one labels those 
-           coordinates lat and long. I think I could do this with only using the second for loop and 
-           getting rid of the first. */
-        /*for (var i = 0; i < 30149; i++){
-            //Creating a variable to hold the coordinates
-            var coord = response.features[0].geometry.coordinates[0][i];
-            //response.features.length
-            //pushing the coordinates into the array:
-            arrayOfCoords.push(coord);
-        }*/
-        //Console.logging the results to see what I got.
-        //console.log(arrayOfCoords);
-
-        //This line will give me the single lat coordinate or if I did 1 in the third post it would be long.
-        //alert(response.features[0].geometry.coordinates[0][0][0])
-
-        //I am working on building an array that will accept the data for the construction of the polygon.
-
-        // var Coords = []
-        //To push into the Coords array, use: 
-        // Coords.push({lat: 1, lng: 2})
-        //This will get the correct coords pushed into the Coords array. 
-        // Coords.push({lat: response.features[0].geometry.coordinates[0][0][1], lng: response.features[0].geometry.coordinates[0][0][0]})
-        // console.log(Coords)
-
-        // for (var i = 0; i < 30149; i++){
-        //     Coords.push({lat: response.features[0].geometry.coordinates[0][i][1], lng: response.features[0].geometry.coordinates[0][i][0]})
-        // }
-
-        //console.log(Coords)
-
-        //This gets me the specific information at one spot: 
-        // console.log(Coords[1].lat);
-
-         // In order to proceede, I believe that I need to convert my arrayOfCoords array to 
-         //    lat and long coordinates. It may even have to be an object that appears to look 
-         //    something like this:
-
-            // var triangleCoords = [
-            //   {lat: 33.965, lng: -84.085},
-            //   {lat: 33.949, lng: -84.049},
-            //   {lat: 33.977, lng: -84.039},
-            //   {lat: 33.983, lng: -84.074}
-            // ];
-        // Once that is done, it can be passed in below under the 'paths:' area. 
-
-        //At this point the coordinates will be placed on the map. 
        
-        //Construct the polygon.
-       
+    } else {
+        console.log("Error: geoCallBack did not get a valid response");
+        return false;
+    }
 };
+
+
+/*
+ * dataCallBack : This is the callback function that responds to the responses 
+ * from the census data based api of the City SDK.
+ *        response: The JSON response from the City SDK
+ */
+function dataCallBack(response) {
+   if (response) {  
+       var dataResults = JSON.stringify(response, null, 4);
+       var resultsObject = JSON.parse(dataResults);
+    
+        DEBUG && console.log(resultsObject.data);
+    } else {
+        console.log("Error: dataCallBack did not get a valid response");
+        return false;
+    }
+    
+   // processTractData(resultsObject);
+}
+
+
     
 /*
  * retrieveData : This function is called by the submit button to connect with the City
- * SDK and obtain the data. 
+ * SDK and obtain the data.
  */
  
 function retrieveData(){
@@ -266,11 +184,12 @@ function retrieveData(){
     request.state = "GA";
     request.sublevel = true;
    
+    
+    
+    
     var checkedVariables = new Array();   // array to hold checked variables
     var numElems = allCheckBoxes.length;  // determines the number of checkboxes
-    // console.log(numElems);
     var foundChecked = false;             // flag for at least one checked box
-
     
     // This loop processes each checkbox and determines which boxes have been
     // checked. The checked boxes are added to the array of checked variables
@@ -278,7 +197,6 @@ function retrieveData(){
         if ( allCheckBoxes[i].checked ) {
            foundChecked = true;
            checkedVariables.push(allCheckBoxes[i].value);
-           console.log(checkedVariables);
         }
     }
 
@@ -287,14 +205,42 @@ function retrieveData(){
         request.variables = checkedVariables;
     else // default vars
         request.variables = ["population", "income"];
+    
+  //  checkLoading();
 
-    // The request to gather the actual data.
-    // We may not need this anymore
-    // census.apiRequest(request, apiCallBack);   
-  
     // This request is used to get geographical data for D3
     census.geoRequest(request, geoCallBack);
-};
+
+    // This request is used to get the data to correlate with the Geo location data.
+    census.apiRequest(request, dataCallBack);
+}
+
+function checkLoading() {
+    if (census.SUPPLEMENTAL_REQUESTS_IN_FLIGHT === 0) {
+        jQuery(".loading-icon-initialstate").hide();
+        return;
+    } else if (census.SUPPLEMENTAL_REQUESTS_IN_FLIGHT > 0) {
+        window.setTimeout(checkLoading, 1500);
+    }
+}
 
 
+function processTractData(resultsDataObject) {
+    
+    if (resultsDataObject) {
+        var numTracts = resultsDataObject.features.length;
+
+        for (var tract = 0; tract < numTracts; tract++) {
+            DEBUG && console.log(resultsDataObject.features[tract].properties.TRACT);
+
+            var numStats = resultsDataObject.totals.length;
+            for (var aStat in resultsDataObject.totals) {
+                if (censusVariables.getVariableFromKey(aStat) !== "Undefined") {
+                    DEBUG && console.log(censusVariables.getVariableFromKey(aStat) + " : " + resultsDataObject.totals[aStat]);
+
+                }
+            }
+        }
+    }
+}
 
