be6dd14eeef28364e4cb0225d8333b41258f0c1e
Carlos Rincon <mezerotm@gmail.com>
2016-09-20 19:40:50 -0400

    Added rightbar selection highlighting

--- js/widgetbar.js
+++ js/widgetbar.js
@@ -1,41 +1,46 @@
-function toggleWidget(widgetbar_tab) {
-
-
-
+// Description: Checks current `widgetbar_tab` for `active` flags and toggles that flag
+function toggleWidget(block, widgetbar_tab) {
   var this_sidebar = document.getElementById(widgetbar_tab);
-
 
   if(this_sidebar.className == "widgetbar-page") {
   	closeActiveSidebars();
     this_sidebar.className += " active";
-    updateMapSize("active");
+    updateMapSize(block, "active");
 	}
   else {
-  	closeActiveSidebars();
-  	updateMapSize("inactive");
+  	closeActiveSidebars(block);
+  	updateMapSize(block, "inactive");
   }
 }
 
+// Description: Cycles through all the `widgetbar-page` classes and removes any `active` flags
 function closeActiveSidebars() {
 	var active_widgetbars = document.getElementsByClassName("widgetbar-page active");
+  var widgetbar_tab_background = document.getElementsByClassName("widgetbar-tab");
+
   	for(var i = 0; i < active_widgetbars.length; i++) {
   		active_widgetbars[i].className = "widgetbar-page";
   	}
+
+    for(var i = 0; i < widgetbar_tab_background.length; i++) {
+  		widgetbar_tab_background[i].style.background = "#f1f3f6";
+  	}
 }
 
-
-
-function updateMapSize(sidebar_state) {
+// Description: Toggles right widget bar pages to display
+function updateMapSize(block, sidebar_state) {
 	var map_holder = document.getElementById("Maps_container");
   var top_panel = document.getElementById("top-panel");
 
 	if(sidebar_state == "active") {
 		map_holder.style.width = "calc(100% - 256px)";
     top_panel.style.width = "calc(100% - 257px)";
+    block.style.background = "#fff";
 	}
 	else {
 		map_holder.style.width = "calc(100% - 44px)";
     top_panel.style.width = "calc(100% - 45px)";
+    block.style.background = "#f1f3f6";
 	}
 	resizeMaps();
 }
